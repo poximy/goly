9189fae5f83053b21bc2be0619a0656a5bc6659a
Erick ZV <zv.erick@protonmail.com>
2021-07-18 19:45:09 -0500

    Add style to Url components

--- src/components/url/Url.svelte
+++ src/components/url/Url.svelte
@@ -10,9 +10,33 @@
 
 <UrlForm miniUrl={(url) => (miniUrl = url)} />
 {#if miniUrl}
-  <a href={miniUrl}>{miniUrl}</a>
-  <button on:click={copy}>COPY</button>
+  <div>
+    <a href={miniUrl}>{miniUrl}</a>
+    <button on:click={copy}>COPY</button>
+  </div>
 {/if}
 
 <style>
+  div {
+    display: flex;
+    justify-content: center;
+  }
+
+  a {
+    color: #1f1f1f;
+    font-size: 1.5em;
+    padding: 0.25em 0.5em;
+    text-decoration: none;
+    background-color: #e6e8e8;
+    border-radius: 0.5em 0 0 0.5em;
+    font-family: "Play", sans-serif;
+  }
+
+  button {
+    border: none;
+    border-radius: 0 0.5em 0.5em 0;
+    background-color: #248f8f;
+    padding: 0.25em 0.5em;
+    font-size: 1.25em;
+  }
 </style>
--- src/components/url/UrlForm.svelte
+++ src/components/url/UrlForm.svelte
@@ -25,4 +25,32 @@
 </form>
 
 <style>
+  form {
+    margin: 25vh 0 3em 0;
+    display: flex;
+    justify-content: center;
+  }
+
+  input {
+    font-size: 1.25em;
+    text-align: center;
+    outline: none;
+    border: none;
+    border-radius: 0.5em 0 0 0.5em;
+    padding: 0 0.5em;
+    font-family: "Play", sans-serif;
+    background-color: #e6e8e8;
+  }
+
+  input::placeholder {
+    font-size: 1.25em;
+    text-align: center;
+  }
+
+  button {
+    border: none;
+    font-size: 1.5em;
+    padding: 0.25em;
+    border-radius: 0 0.5em 0.5em 0;
+  }
 </style>
