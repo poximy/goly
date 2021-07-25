416452124c74e703087b6dadb90d9edeb19f0076
Erick ZV <zv.erick@protonmail.com>
2021-07-24 21:44:52 -0500

    Add page & token handler

--- src/App.svelte
+++ src/App.svelte
@@ -2,32 +2,37 @@
   import Url from "./components/url/Url.svelte";
   import Nav from "./components/nav/Nav.svelte";
 
+  import Auth from "./components/auth/Auth.svelte";
+
   let jwtToken: string = "";
   let page: page = "url";
   let backBtn = false;
 
-  const urls = () => {
+  const pageHandler = () => {
     if (backBtn) {
       page = "url";
-      backBtn = false;
+      backBtn = false
+    } else if (jwtToken === "") {
+      page = "auth";
+      backBtn = true;
     } else {
-      if (jwtToken === "") {
-        page = "auth";
-      } else {
-        page = "metadata";
-      }
+      page = "metadata";
       backBtn = true;
     }
   };
+
+  const tokenHandler = (token: string) => {
+    jwtToken = token;
+  };
 </script>
 
-<Nav {urls} {backBtn} />
+<Nav urls={pageHandler} {backBtn} />
 <main>
   {#if page === "url"}
     <Url />
-  {:else if page === "auth"}
-    <p>auth</p>
-  {:else if page === "metadata"}
+  {:else if jwtToken === ""}
+    <Auth {tokenHandler} />
+  {:else}
     <p>metadata</p>
   {/if}
 </main>
