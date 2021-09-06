9b9fceb7711597788cfcc1235f92bf997ef624d3
Erick ZV <zv.erick@protonmail.com>
2021-09-06 17:33:08 -0500

    Add deleteUrl function

--- src/api/url.ts
+++ src/api/url.ts
@@ -2,7 +2,7 @@ interface UrlID {
   _id: string;
 }
 
-const minifyUrl = async (urlText: string) => {
+export const minifyUrl = async (urlText: string) => {
   const bodyData = { url: urlText };
   const apiUrl = window.location.origin;
 
@@ -20,4 +20,19 @@ const minifyUrl = async (urlText: string) => {
   }
 };
 
-export default minifyUrl;
+export const deleteUrl = async (token: string, url_id: string) => {
+  const apiUrl = window.location.origin + url_id;
+  try {
+    const res = await fetch(apiUrl, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
