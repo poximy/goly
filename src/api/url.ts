0ae43a3d5e4f37724ca0ddc3005fe85c06c5bbbc
Erick ZV <zv.erick@protonmail.com>
2021-09-10 09:55:58 -0500

    Change apiUrl location

--- src/api/url.ts
+++ src/api/url.ts
@@ -7,9 +7,10 @@ interface UrlHeader {
   Authorization?: string;
 }
 
+const apiUrl = process.env.API_URL;
+
 export const minifyUrl = async (urlText: string, token: string = "") => {
   const bodyData = { url: urlText };
-  const apiUrl = process["env"].API_URL + "/";
 
   const headers: UrlHeader = { "content-type": "application/json" };
 
@@ -18,7 +19,7 @@ export const minifyUrl = async (urlText: string, token: string = "") => {
   }
 
   try {
-    const res = await fetch(apiUrl, {
+    const res = await fetch(apiUrl + "/", {
       method: "POST",
       headers: { ...headers },
       body: JSON.stringify(bodyData),
@@ -32,9 +33,8 @@ export const minifyUrl = async (urlText: string, token: string = "") => {
 };
 
 export const deleteUrl = async (token: string, url_id: string) => {
-  const apiUrl = process["env"].API_URL + "/" + url_id;
   try {
-    const res = await fetch(apiUrl, {
+    const res = await fetch(apiUrl + "/" + url_id, {
       method: "DELETE",
       headers: {
         Accept: "application/json",
