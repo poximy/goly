33c7c3240c806a210bc1ece3f2e5c892bff75915
poximy <76846708+poximy@users.noreply.github.com>
2022-05-05 07:35:06 -0500

    fix(navbar): change output format

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,31 +3,34 @@ import type { FC } from 'react';
 
 const Nav: FC = () => {
   return (
-    <div
-      className="absolute top-0 z-10 my-8 grid w-full grid-cols-2 items-center
-			gap-y-4 font-sans text-xl text-white sm:gap-y-8 md:grid-cols-3"
+    <nav
+      className="absolute top-0 z-10 my-8 grid w-full grid-cols-2 gap-y-4
+			text-center font-sans text-white md:grid-cols-3"
     >
-      <nav className="col-span-2 w-fit place-self-center text-center text-4xl font-bold sm:col-span-1">
-        <Link href="/">
-          <a>
-            <h1>Minify URL</h1>
-          </a>
-        </Link>
-      </nav>
-
-      <div className="col-span-2 flex justify-center gap-x-4 text-center font-sans sm:col-span-1 md:col-start-3">
+      <Link href="/">
+        <a
+          className="col-span-2 w-fit self-center justify-self-center text-4xl
+					font-bold leading-none sm:col-span-1 md:col-start-2"
+        >
+          <h1>Minify URL</h1>
+        </a>
+      </Link>
+      <div
+        className="col-span-2 flex flex-row items-center justify-center gap-x-4
+				text-center text-xl sm:col-span-1 md:col-start-3"
+      >
         <Link href="/login">
-          <a className="h-[48px] w-24 border hover:bg-white hover:text-black items">
+          <a className="h-[48px] w-24 rounded border py-2 hover:bg-white hover:text-black">
             Log in
           </a>
         </Link>
         <Link href="/signup">
-          <a className="h-[48px] w-24 border hover:bg-white hover:text-black">
+          <a className="h-[48px] w-24 rounded border py-2 hover:bg-white hover:text-black">
             Sign up
           </a>
         </Link>
       </div>
-    </div>
+    </nav>
   );
 };
 
