0c2d59563421fae228858e5a4af9ed243a0ba59f
poximy <76846708+poximy@users.noreply.github.com>
2022-06-28 09:34:24 -0500

    style: redo navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,42 +1,34 @@
 <script lang="ts">
-  // <nav
-  //   className='absolute top-0 z-10 my-8 flex h-[48px] w-full items-center
-  // 	justify-center text-white'
-  // >
-  //     <a href="/" className='font-sans text-4xl font-bold leading-none'>
-  //       <h1>Minify URL</h1>
-  //     </a>
-  // </nav>
 </script>
 
 <nav
-  class="absolute top-0 z-10 my-8 grid w-full grid-cols-2 gap-y-4 text-center
-  font-sans text-darker dark:text-light md:grid-cols-3"
+  class="fixed top-0 z-10  grid w-full grid-cols-2 gap-y-2 border-darker bg-dark
+  py-2 text-center font-sans text-light shadow-md md:grid-cols-3"
 >
   <a
     href="/"
-    class="col-span-2 w-fit self-center justify-self-center font-mono text-4xl
-    font-bold leading-none sm:col-span-1 md:col-start-2"
+    class="col-span-2 w-fit self-center justify-self-center font-mono text-2xl
+    font-bold leading-none sm:col-span-1 sm:text-4xl md:col-start-2"
   >
     <h1>Goly</h1>
   </a>
   <div
     class="col-span-2 flex flex-row items-center justify-center gap-x-4
-    text-center text-xl sm:col-span-1 md:col-start-3"
+    text-center text-base sm:col-span-1 sm:text-xl md:col-start-3"
   >
     <a
       href="/login"
-      class="h-[48px] w-24 rounded border border-dark py-2
-			transition-all duration-[600ms] hover:bg-dark hover:text-light
-		  hover:duration-100 dark:border-light dark:hover:bg-light dark:hover:text-dark"
+      class="h-[32px] w-16 rounded border border-light py-0.5 transition-all
+      duration-[500ms] hover:bg-light hover:text-dark hover:duration-100
+      sm:h-[48px] sm:w-24 sm:py-2"
     >
       Log in
     </a>
     <a
       href="signup"
-      class="h-[48px] w-24 rounded border border-dark py-2
-			transition-all duration-[600ms] hover:bg-dark hover:text-light
-			hover:duration-100 dark:border-light dark:hover:bg-light dark:hover:text-dark"
+      class="h-[32px] w-16 rounded border border-light py-0.5 transition-all
+      duration-[500ms] hover:bg-light hover:text-dark hover:duration-100
+      sm:h-[48px] sm:w-24 sm:py-2"
     >
       Sign up
     </a>
