f5eb719617123842ce7629ac984ababcec6623c5
poximy <76846708+poximy@users.noreply.github.com>
2022-06-28 09:36:32 -0500

    style: redo home screeen

--- src/App.svelte
+++ src/App.svelte
@@ -25,14 +25,15 @@
   $: if (errorMessage !== '') {
     setTimeout(() => {
       errorMessage = '';
-    }, 5000);
+    }, 3000);
   }
 </script>
 
 <Nav />
 
 <main
-  class="relative flex h-screen w-full flex-col flex-wrap items-center justify-center"
+  class="relative flex h-screen w-full flex-col flex-wrap items-center
+  justify-center gap-4 text-xl"
 >
   {#if errorMessage !== ''}
     <div
@@ -46,71 +47,77 @@
       out:fade
       on:click={() => (errorMessage = '')}
     >
-      <p class="truncate font-sans text-2xl font-bold capitalize text-light">
+      <p class="truncate font-sans text-xl font-bold capitalize text-light">
         {errorMessage}!
       </p>
     </div>
   {/if}
-  <form on:submit|preventDefault={handleSubmit}>
-    <div class="relative flex justify-center">
-      {#if golyUrl !== ''}
-        <div
-          class="absolute -top-14 flex flex-row items-center justify-center gap-4
-        font-mono text-lg sm:text-2xl"
-        >
-          <a
-            href={golyUrl}
-            target="_blank"
-            rel="noreferrer"
-            class="text-dark underline-offset-2 hover:underline dark:text-light"
-          >
-            <p>
-              {golyUrl}
-            </p>
-          </a>
-          <button
-            type="button"
-            on:click={() => console.log('copy')}
-            class="box-border aspect-video h-12 rounded border border-dark p-2
-					font-sans text-xl font-bold text-dark transition-all duration-500
-					hover:bg-dark hover:text-light hover:duration-100 dark:border-light
-					dark:text-light dark:hover:bg-light dark:hover:text-dark"
-          >
-            Copy
-          </button>
-        </div>
-      {/if}
-      <input
-        id="url"
-        type="text"
-        bind:value={url}
-        placeholder="Insert URL..."
-        class="h-12 w-72 rounded-l border border-r-0 border-dark bg-light px-2
-      font-mono text-xl focus:outline-none dark:border-none sm:h-14 sm:w-96
-			sm:px-4 md:w-[32rem] lg:w-[48rem]"
-      />
+  {#if golyUrl !== ''}
+    <div
+      class="absolute top-1/3 flex -translate-y-1/3 flex-col items-center
+      justify-center gap-4 rounded font-mono text-xl sm:flex-row"
+    >
+      <a
+        href={golyUrl}
+        target="_blank"
+        rel="noreferrer"
+        class="px-2 text-light underline-offset-2 hover:underline"
+      >
+        {golyUrl}
+      </a>
       <button
-        type="submit"
-        class="box-border aspect-square h-12 rounded-r border border-l-0
-		  border-dark bg-primary p-2 hover:bg-[#2363be] dark:border-none sm:h-14"
+        type="button"
+        on:click={() => console.log('copy')}
+        class="group box-border aspect-square h-12 rounded border p-2 hover:bg-light sm:h-14"
       >
         <svg
-          class="h-8 w-8 stroke-white sm:h-10 sm:w-10"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
+          class="h-8 w-8 stroke-light group-hover:stroke-dark sm:h-10 sm:w-10"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width={1}
         >
           <path
-            stroke="2"
             stroke-linecap="round"
             stroke-linejoin="round"
-            d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656
-          5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0
-          00-5.656-5.656l-1.1 1.1"
+            d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0
+            002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
           />
         </svg>
       </button>
     </div>
+  {/if}
+  <form on:submit|preventDefault={handleSubmit} class="flex justify-center">
+    <input
+      id="url"
+      type="text"
+      bind:value={url}
+      placeholder="Insert URL..."
+      class="h-12 w-72 rounded-l border-none bg-light px-2 font-mono
+      focus:outline-none sm:h-14 sm:w-96 sm:px-4 md:w-[32rem] lg:w-[48rem]"
+    />
+    <button
+      type="submit"
+      class="box-border aspect-square h-12 rounded-r
+      border-none bg-primary p-2 hover:bg-[#2363be] sm:h-14"
+    >
+      <svg
+        class="h-8 w-8 stroke-white sm:h-10 sm:w-10"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656
+          5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0
+          00-5.656-5.656l-1.1 1.1"
+        />
+      </svg>
+    </button>
   </form>
 </main>
